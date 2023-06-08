72c6108be40074c9a75940af14afd2b7be5ed8f6
ToroERP222 <135905667+ToroERP222@users.noreply.github.com>
2023-06-08 11:15:38 -0600

    changes on api call

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home({}) {
         try {
           const response = await axios.get(`${process.env.IP}/api/v1/auth/me?token=${cookies.token}`);
   
-          const json = await response.json();
-          const jsonData = json.data;
+          
+          console.log(response.data)
           if (jsonData[0] === undefined) {
             // User not logged in
             return(    <div  style={{height: '100vh',
