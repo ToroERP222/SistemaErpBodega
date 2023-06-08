ad724a5ca041eeba88bf218259a8bd4537a6cf2f
ToroERP222 <135905667+ToroERP222@users.noreply.github.com>
2023-06-08 11:39:42 -0600

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,103 @@
-import { useState } from 'react';
-import Head from 'next/head';
+
+import { useState,useEffect } from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import Login from './login';
-import Layout from '../components/Layout';
-import LayoutE from '../components/Layout/LayoutEmpleados';
-import { useCookies } from 'react-cookie';
+import cookie from 'js-cookie';
+import Link from 'next/link'
+import Router from 'next/router';
 
-const Home = ({ data }) => {
-  const [cookies, setCookie] = useCookies(['token']);
-  const [user, setUser] = useState(data && data[0] ? data[0].nombre : null);
+import Login from './login'
 
-  return (
-    <div
-      style={{
-        height: '100vh',
+import Layout from '../components/Layout'
+import LayoutE from '../components/Layout/LayoutEmpleados'
+export default function Home({data}) {
+  const [user, setuser] = useState(null)
+    console.log(data)
+    useEffect(() => {
+      if (data[0]=== undefined) {
+        return(    <div  style={{height: '100vh',
         position: 'relative',
         backgroundSize: 'cover',
-        backgroundImage: `url('/backg.jpg')`,
-      }}
-    >
-      <div>
-        <Head>
-          <title>Comercialización</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        {!user && <Login />}
-        {user && data && data[0].role === 'admin' && <Layout user={user} />}
-        {user && data && data[0].role === 'vendedor' && <LayoutE user={user} />}
-        {user && data && data[0].role === 'promotor' && <Layout />}
-      </div>
-    </div>
-  );
-};
+        backgroundImage: `url('/backg.jpg')`}}>
+          <Login/>
+        </div>)
+    
+  
+  
+      
+    }else{
+      setuser(data[0].nombre)
+    }
+    
+      
+    }, [data])
+    
+   
 
+ 
+
+     const [info, setinfo] = useState({})
+      let vendedor;
+      let admin
+      let promotor;
+      let almacen;
+      let init = false
+      if(data[0] != undefined){
+        if(data[0].role === 'vendedor'){
+        vendedor= true
+        init = true
+      }else if(data[0].role === 'admin'||'distribuidor'||'almacen'){
+        admin = true
+        init = true
+      }else if(data[0].role === 'promotor'){
+        promotor = true
+        init = true
+      }
+    }
+      
+      
+    
+      
+
+  return (
+    <div  style={{height: '100vh',
+      position: 'relative',
+      backgroundSize: 'cover',
+      backgroundImage: `url('/backg.jpg')`}}>
+       
+     <div>
+      <Head>
+        <title>Comercialización</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {!init && (
+        <><Login/></>
+      )}
+      {admin && (
+        <>
+          <Layout user={user}/>
+        </>
+      )}
+      { vendedor && (
+        <>
+          <LayoutE user={user}/>
+        </>
+      )}
+       { promotor && (
+        <>
+          <Layout/>
+        </>
+      )}
+
+    </div>
+    </div>
+  )
+}
 Home.getInitialProps = async (ctx) => {
   const { req, res } = ctx;
-  const { token } = req.cookies;
+  const token = req && req.cookies ? req.cookies.token : null;
 
   if (token) {
     try {
