b44d910fdd82fda7a773ed43d4307423ee580b98
ToroERP222 <135905667+ToroERP222@users.noreply.github.com>
2023-06-08 10:56:14 -0600

    changes

--- pages/index.js
+++ pages/index.js
@@ -14,108 +14,68 @@ import Layout from '../components/Layout'
 import LayoutE from '../components/Layout/LayoutEmpleados'
 import { useCookies } from 'react-cookie';
 
-export default function Home({data}) {
+export default function Home({}) {
   const [user, setuser] = useState(null)
   const [cookies, setCookie] = useCookies(['token']);
+  const [data, setData] = useState(null);
     console.log(cookies)
     useEffect(() => {
-      if (data[0]=== undefined) {
-        return(    <div  style={{height: '100vh',
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(`${process.env.IP}/api/v1/auth/me`, {
+            headers: {
+              cookie: cookies, // Replace 'cookieName' with the actual name of your cookie
+            },
+          });
+  
+          const json = await response.json();
+          const jsonData = json.data;
+          if (jsonData[0] === undefined) {
+            // User not logged in
+            return(    <div  style={{height: '100vh',
         position: 'relative',
         backgroundSize: 'cover',
         backgroundImage: `url('/backg.jpg')`}}>
           <Login/>
         </div>)
-    
+          }
   
+          setUser(jsonData[0].nombre);
+          setData(jsonData);
+        } catch (error) {
+          console.error(error);
+        }
+      };
   
-      
-    }else{
-      setuser(data[0].nombre)
-    }
-    
-      
-    }, [data])
+      fetchData();
+    }, [data]);
+   
     
    
 
  
 
-     const [info, setinfo] = useState({})
-      let vendedor;
-      let admin
-      let promotor;
-      let almacen;
-      let init = false
-      if(data[0] != undefined){
-        if(data[0].role === 'vendedor'){
-        vendedor= true
-        init = true
-      }else if(data[0].role === 'admin'||'distribuidor'||'almacen'){
-        admin = true
-        init = true
-      }else if(data[0].role === 'promotor'){
-        promotor = true
-        init = true
-      }
-    }
-      
-      
-    
-      
-
+     
   return (
-    <div  style={{height: '100vh',
-      position: 'relative',
-      backgroundSize: 'cover',
-      backgroundImage: `url('/backg.jpg')`}}>
-       
-     <div>
-      <Head>
-        <title>Comercialización</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      {!init && (
-        <><Login/></>
-      )}
-      {admin && (
-        <>
-          <Layout user={user}/>
-        </>
-      )}
-      { vendedor && (
-        <>
-          <LayoutE user={user}/>
-        </>
-      )}
-       { promotor && (
-        <>
-          <Layout/>
-        </>
-      )}
-
+    <div
+      style={{
+        height: '100vh',
+        position: 'relative',
+        backgroundSize: 'cover',
+        backgroundImage: `url('/backg.jpg')`,
+      }}
+    >
+      <div>
+        <Head>
+          <title>Comercialización</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        {!data && <Login />}
+        {data && data[0].role === 'admin' && <Layout user={user} />}
+        {data && data[0].role === 'vendedor' && <LayoutE user={user} />}
+        {data && data[0].role === 'promotor' && <Layout />}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-Home.getInitialProps = async (ctx) => {
-  const cookie = ctx.req ? ctx.req.headers.cookie : null
-  var result = [];
 
-  
-    const data =await fetch(`${process.env.IP}/api/v1/auth/me`,{
-      method: 'GET',
-      headers: {
-        cookie
-    }
-    })
-   
-
-  const json = await data.json()
-  
-  const jsondta = json.data
-  for(var i in jsondta)
-    result.push(jsondta [i])
-
-  return { data: result }
-}
