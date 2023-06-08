3693c1373ccd1bc5ce7fd8ca7e4634b2f7e9d735
ToroERP222 <135905667+ToroERP222@users.noreply.github.com>
2023-06-08 11:35:12 -0600

    changes on login

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,15 @@
-
-import { useState,useEffect } from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import Head from 'next/head';
 import axios from 'axios';
-import cookie from 'js-cookie';
-import Link from 'next/link'
-import Router from 'next/router';
-
-import Login from './login'
-
-import Layout from '../components/Layout'
-import LayoutE from '../components/Layout/LayoutEmpleados'
+import Login from './login';
+import Layout from '../components/Layout';
+import LayoutE from '../components/Layout/LayoutEmpleados';
 import { useCookies } from 'react-cookie';
 
-export default function Home({}) {
-  
+export default function Home({ data }) {
   const [cookies, setCookie] = useCookies(['token']);
-  const [data, setData] = useState(null);
-  const [user, setUser] = useState(null);
-    console.log(cookies)
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(`${process.env.IP}/api/v1/auth/me?token=${cookies.token}`);
-  
-          const json = await response.data
-          const jsonData = json.data;
-          if (jsonData[0] === undefined) { 
-            // User not logged in
-            return(    <div  style={{height: '100vh',
-        position: 'relative',
-        backgroundSize: 'cover',
-        backgroundImage: `url('/backg.jpg')`}}>
-          <Login/>
-        </div>)
-          }
-  
-          setUser(jsonData[0].nombre);
-          setData(jsonData);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-  
-      fetchData();
-    }, []);
-   
-    
-   
+  const [user, setUser] = useState(data && data[0] ? data[0].nombre : null);
 
- 
-
-     
   return (
     <div
       style={{
@@ -67,12 +24,48 @@ export default function Home({}) {
           <title>Comercialización</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        {!data && <Login />}
-        {data && data[0].role === 'admin' && <Layout user={user} />}
-        {data && data[0].role === 'vendedor' && <LayoutE user={user} />}
-        {data && data[0].role === 'promotor' && <Layout />}
+        {!user && <Login />}
+        {user && data && data[0].role === 'admin' && <Layout user={user} />}
+        {user && data && data[0].role === 'vendedor' && <LayoutE user={user} />}
+        {user && data && data[0].role === 'promotor' && <Layout />}
       </div>
     </div>
   );
 }
 
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const { token } = req.cookies;
+
+  if (token) {
+    try {
+      const response = await axios.get(`${process.env.IP}/api/v1/auth/me?token=${token}`);
+      const json = await response.data;
+      const jsonData = json.data;
+
+      if (jsonData[0] === undefined) {
+        // User not logged in
+        return {
+          props: {
+            data: null,
+          },
+        };
+      } else {
+        return {
+          props: {
+            data: jsonData,
+          },
+        };
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // User not logged in
+  return {
+    props: {
+      data: null,
+    },
+  };
+}
