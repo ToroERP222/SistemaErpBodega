921d1788d4f91381f9ecf671f7d694a8500e8c1c
ToroERP222 <135905667+ToroERP222@users.noreply.github.com>
2023-06-08 10:31:44 -0600

    add react-cookie

--- pages/index.js
+++ pages/index.js
@@ -12,65 +12,107 @@ import Login from './login'
 
 import Layout from '../components/Layout'
 import LayoutE from '../components/Layout/LayoutEmpleados'
-export default function Home() {
-  const [user, setUser] = useState(null);
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${process.env.IP}/api/v1/auth/me`,{
-          method: 'GET',
-          headers: {
-            cookie: cookie.get('token')
-        }
-        })
-        const json = await response.json();
-        const jsonData = json.data;
-        if (jsonData[0] === undefined) {
-          return(    <div  style={{height: '100vh',
-          position: 'relative',
-          backgroundSize: 'cover',
-          backgroundImage: `url('/backg.jpg')`}}>
-            <Login/>
-          </div>)
-        }
-
-        setUser(jsonData[0].nombre);
-        setData(jsonData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
- 
+const [cookies, setCookie] = useCookies(['token']);
+export default function Home({data}) {
+  const [user, setuser] = useState(null)
+    console.log(data)
+    useEffect(() => {
+      if (data[0]=== undefined) {
+        return(    <div  style={{height: '100vh',
+        position: 'relative',
+        backgroundSize: 'cover',
+        backgroundImage: `url('/backg.jpg')`}}>
+          <Login/>
+        </div>)
+    
+  
+  
+      
+    }else{
+      setuser(data[0].nombre)
+    }
+    
+      
+    }, [data])
+    
    
 
  
 
-  return (
-    <div
-      style={{
-        height: '100vh',
-        position: 'relative',
-        backgroundSize: 'cover',
-        backgroundImage: `url('/backg.jpg')`,
-      }}
-    >
-      <div>
-        <Head>
-          <title>Comercialización</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        {!data && <Login />}
-        {data && data[0].role === 'admin' && <Layout user={user} />}
-        {data && data[0].role === 'vendedor' && <LayoutE user={user} />}
-        {data && data[0].role === 'promotor' && <Layout />}
-      </div>
-    </div>
-  );
-}
+     const [info, setinfo] = useState({})
+      let vendedor;
+      let admin
+      let promotor;
+      let almacen;
+      let init = false
+      if(data[0] != undefined){
+        if(data[0].role === 'vendedor'){
+        vendedor= true
+        init = true
+      }else if(data[0].role === 'admin'||'distribuidor'||'almacen'){
+        admin = true
+        init = true
+      }else if(data[0].role === 'promotor'){
+        promotor = true
+        init = true
+      }
+    }
+      
+      
+    
+      
 
+  return (
+    <div  style={{height: '100vh',
+      position: 'relative',
+      backgroundSize: 'cover',
+      backgroundImage: `url('/backg.jpg')`}}>
+       
+     <div>
+      <Head>
+        <title>Comercialización</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {!init && (
+        <><Login/></>
+      )}
+      {admin && (
+        <>
+          <Layout user={user}/>
+        </>
+      )}
+      { vendedor && (
+        <>
+          <LayoutE user={user}/>
+        </>
+      )}
+       { promotor && (
+        <>
+          <Layout/>
+        </>
+      )}
+
+    </div>
+    </div>
+  )
+}
+Home.getInitialProps = async (ctx) => {
+
+
+  
+    const data =await fetch(`${process.env.IP}/api/v1/auth/me`,{
+      method: 'GET',
+      headers: {
+        cookies
+    }
+    })
+   
+
+  const json = await data.json()
+  
+  const jsondta = json.data
+  for(var i in jsondta)
+    result.push(jsondta [i])
+
+  return { data: result }
+}
